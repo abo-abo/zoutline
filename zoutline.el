2232139b64f9adabe81f48dd8d2f059e3f19edda
Oleh Krehel <ohwoeowho@gmail.com>
2019-02-11 17:51:23 +0100

    zoutline.el (zo-bnd-subtree): Save point position on error
    Fixes abo-abo/lpy#22

--- zoutline.el
+++ zoutline.el
@@ -154,20 +154,21 @@ Return nil if moved 0 times."
 
 (defun zo-bnd-subtree ()
   "Return a cons of heading end and subtree end."
-  (condition-case nil
-      (progn
-        (outline-back-to-heading)
-        (cons
-         (save-excursion
-           (outline-end-of-heading)
-           (point))
-         (save-excursion
-           (zo-end-of-subtree)
-           (when (bolp)
-             (backward-char))
-           (point))))
-    (error
-     (cons (point-min) (point-max)))))
+  (save-excursion
+    (condition-case nil
+        (progn
+          (outline-back-to-heading)
+          (cons
+           (save-excursion
+             (outline-end-of-heading)
+             (point))
+           (save-excursion
+             (zo-end-of-subtree)
+             (when (bolp)
+               (backward-char))
+             (point))))
+      (error
+       (cons (point-min) (point-max))))))
 
 (provide 'zoutline)
 
